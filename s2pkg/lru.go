cac0e298a3d14af9e14b81656d6b3613baca9b4b
coyove <coyove@hotmail.com>
2022-02-16 13:42:09 +0800

    master LRU

--- s2pkg/lru.go
+++ s2pkg/lru.go
@@ -382,3 +382,184 @@ func (c *KeyedLRUCache) Info(callback func(*KeyedCacheItem, int64, int64)) {
 	}
 	c.Unlock()
 }
+
+type lruValue struct {
+	master     string
+	slaves     map[string]struct{}
+	slaveStore interface{}
+}
+
+type LRUKeyValue struct {
+	Master string
+	Slave  string
+	Value  interface{}
+}
+
+type MasterLRU struct {
+	mu        sync.RWMutex
+	onEvict   func(LRUKeyValue)
+	lruCap    int64
+	lenMax    int64
+	hot, cold map[string]lruValue
+}
+
+func NewMasterLRU(cap int64, onEvict func(LRUKeyValue)) *MasterLRU {
+	if cap <= 0 {
+		cap = 1
+	}
+	if onEvict == nil {
+		onEvict = func(LRUKeyValue) {}
+	}
+	return &MasterLRU{
+		onEvict: onEvict,
+		lruCap:  cap,
+		hot:     map[string]lruValue{},
+		cold:    map[string]lruValue{},
+	}
+}
+
+func (m *MasterLRU) Len() int { return len(m.hot) + len(m.cold) }
+
+func (m *MasterLRU) Cap() int { return int(m.lenMax) }
+
+func (m *MasterLRU) Add(masterKey, slaveKey string, value interface{}) {
+	if slaveKey == "" {
+		panic("slave key can't be empty")
+	}
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if masterKey == "" {
+		m.hot[slaveKey] = lruValue{slaveStore: value}
+		delete(m.cold, slaveKey)
+	} else {
+		master := m.getByKey(masterKey)
+		if master.slaves != nil {
+			master.slaves[slaveKey] = struct{}{}
+		} else {
+			master = lruValue{slaves: map[string]struct{}{slaveKey: {}}}
+		}
+		m.hot[masterKey] = master
+		m.hot[slaveKey] = lruValue{master: masterKey, slaveStore: value}
+		delete(m.cold, masterKey)
+		delete(m.cold, slaveKey)
+	}
+
+	sz := int64(m.Len())
+	if sz > m.lenMax {
+		m.lenMax = sz
+	}
+	if sz > m.lruCap && len(m.hot) > len(m.cold)*2/3 {
+		for k, v := range m.cold {
+			m.delete(k, v, true, true)
+			if int64(m.Len()) <= m.lruCap {
+				break
+			}
+		}
+		if len(m.cold) == 0 {
+			m.hot, m.cold = m.cold, m.hot
+		}
+	}
+}
+
+func (m *MasterLRU) Get(key string) (interface{}, bool) {
+	e, ok := m.find(key)
+	return e.slaveStore, ok
+}
+
+func (m *MasterLRU) find(k string) (lruValue, bool) {
+	m.mu.RLock()
+	v, ok := m.hot[k]
+	m.mu.RUnlock()
+	if ok {
+		return v, true
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	v, ok = m.cold[k]
+	if ok {
+		m.hot[k] = v
+		delete(m.cold, k)
+		return v, true
+	}
+	return lruValue{}, false
+}
+
+func (m *MasterLRU) getByKey(key string) lruValue {
+	x, ok := m.hot[key]
+	if !ok {
+		x = m.cold[key]
+	}
+	return x
+}
+
+func (m *MasterLRU) Delete(key string) int {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	return m.delete(key, lruValue{}, false, false)
+}
+
+func (m *MasterLRU) delete(key string, value lruValue, valueProvided, emit bool) int {
+	cnt := 0
+	if !valueProvided {
+		value = m.getByKey(key)
+	}
+	if value.slaves != nil {
+		// To delete a master key, delete all slaves of it
+		for slave := range value.slaves {
+			cnt++
+			if emit {
+				m.onEvict(LRUKeyValue{key, slave, m.getByKey(slave).slaveStore})
+			}
+			delete(m.hot, slave)
+			delete(m.cold, slave)
+		}
+	} else {
+		// To delete a slave key, remove it from its master's records
+		if value.master != "" {
+			master := m.getByKey(value.master)
+			if master.slaves == nil {
+				panic("missing master on deletion")
+			}
+			delete(master.slaves, key)
+		}
+		if emit {
+			m.onEvict(LRUKeyValue{value.master, key, value.slaveStore})
+		}
+	}
+	delete(m.hot, key)
+	delete(m.cold, key)
+	return 1 + cnt
+}
+
+func (m *MasterLRU) Range(f func(LRUKeyValue) bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	for _, m := range []map[string]lruValue{m.hot, m.cold} {
+		for k, v := range m {
+			if v.slaves != nil {
+				continue
+			}
+			if !f(LRUKeyValue{v.master, k, v.slaveStore}) {
+				return
+			}
+		}
+	}
+}
+
+func (m *MasterLRU) RangeMaster(f func(string, map[string]struct{}) bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	for _, m := range []map[string]lruValue{m.hot, m.cold} {
+		for k, v := range m {
+			if v.slaves == nil {
+				continue
+			}
+			if !f(k, v.slaves) {
+				return
+			}
+		}
+	}
+}
--- s2pkg/metrics_test.go
+++ s2pkg/metrics_test.go
@@ -3,6 +3,7 @@ package s2pkg
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -68,6 +69,87 @@ func TestMetrics2(t *testing.T) {
 	fmt.Println(s.Value)
 	fmt.Println(s.Count)
 	fmt.Println(s.Ts)
+}
+
+func TestDoubleMapLRU(t *testing.T) {
+	type op struct {
+		add bool
+		v   int
+	}
+	ops := []op{}
+
+	const N = 2048
+	m := NewMasterLRU(N, nil)
+	r := NewLRUCache(N)
+
+	for i := 0; i < 1e5; i++ {
+		ops = append(ops, op{true, i})
+		if i > 10 && rand.Intn(2) == 0 {
+			cnt := rand.Intn(60) + 20
+			for z := 0; z < cnt; z++ {
+				x := rand.Intn(i)
+				ops = append(ops, op{false, x})
+			}
+		}
+	}
+
+	start := time.Now()
+	for _, op := range ops {
+		if op.add {
+			m.Add("", strconv.Itoa(op.v), op.v)
+		} else {
+			m.Get(strconv.Itoa(op.v))
+		}
+	}
+	hcDiff := time.Since(start)
+
+	start = time.Now()
+	for _, op := range ops {
+		if op.add {
+			r.Add(strconv.Itoa(op.v), op.v)
+		} else {
+			r.Get(strconv.Itoa(op.v))
+		}
+	}
+	refDiff := time.Since(start)
+
+	miss := 0
+	r.Info(func(k LRUKey, v interface{}, a int64, b int64) {
+		_, ok := m.Get(k.(string))
+		// fmt.Println("ref", k, tv)
+		if !ok {
+			miss++
+		}
+	})
+	fmt.Println(r.Len(), miss, m.Cap(), "diff:", hcDiff, refDiff)
+
+	const M = 1e4
+	for i := 0; i < 1e4; i++ {
+		start := time.Now()
+		const cap = 100
+		m = NewMasterLRU(cap, nil)
+		for i := 0; i < 1000; i++ {
+			m.Add("master", strconv.Itoa(i), i)
+		}
+		cnt := m.Delete("master")
+		if i == M-1 {
+			fmt.Println("delete master: ", cnt, m.Cap(), time.Since(start)*M)
+		}
+		if m.Len() != 0 {
+			t.Fatal(m.Len())
+		}
+	}
+
+	m = NewMasterLRU(1, func(kv LRUKeyValue) {
+		fmt.Println(kv.Slave)
+	})
+	for i := 0; i < 7; i++ {
+		m.Add("", strconv.Itoa(i), i)
+	}
+	m.Range(func(kv LRUKeyValue) bool {
+		fmt.Println(kv.Slave, "=", kv.Value)
+		return true
+	})
 }
 
 func TestMatch(t *testing.T) {
